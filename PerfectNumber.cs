59274b2d900cee2e54fe0adf538996dc7416ca03
preeti <pritich0896@gmail.com>
2022-12-31 20:00:40 +0530

    added Perfect  number

---
+++ PerfectNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class PerfectNumber
+    {
+        public static void Display()
+        {
+            int sum = 0, N;
+            Console.WriteLine("Enter the number: ");
+            int no = Convert.ToInt32(Console.ReadLine());
+            N = no;
+            for (int i = 1; i < no; i++)
+            {
+                if (no % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == N)
+            {
+                Console.WriteLine(" Entered number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine(" Entered number is not perfect");
+            }
+            Console.ReadLine();
+        }
+    }
+}
