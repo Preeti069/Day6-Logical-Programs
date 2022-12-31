d29ac55389ac6135abb86da8db260a72e19b2cb4
preeti <pritich0896@gmail.com>
2022-12-31 19:43:55 +0530

    fibonacciseries

---
+++ FibonacciSeries.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibonacciSeries
+    {
+        public static void DisplayFibo()
+        {
+
+            int n1 = 0, n2 = 1, n3, i, no;
+
+            Console.WriteLine("Enter the number of element");
+
+            no = Convert.ToInt32(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (i = 2; i < no; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
